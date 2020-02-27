899ff1e9a018f3856b201cc6d31eeaea6e82b470
Summer Ji <summerji@google.com>
2020-02-27 15:38:30 -0800

    chore: update .jsdoc.js by add protos and remove double quotes (#273)

--- .jsdoc.js
+++ .jsdoc.js
@@ -31,7 +31,8 @@ module.exports = {
   source: {
     excludePattern: '(^|\\/|\\\\)[._]',
     include: [
-      'src'
+      'src',
+      'protos'
     ],
     includePattern: '\\.js$'
   },
@@ -42,7 +43,7 @@ module.exports = {
     systemName: 'gtoken',
     theme: 'lumen',
     default: {
-      "outputSourceFiles": false
+      outputSourceFiles: false
     }
   },
   markdown: {
